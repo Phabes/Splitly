a9a192f45cbf9216de3978b1849b9cda58ddf143
Phabes <wojciechlacki.praca@gmail.com>
2026-01-11 19:43:35 +0100

    Unnecessary code removed.

--- app/app/components/index.ts
+++ app/app/components/index.ts
@@ -6,7 +6,6 @@ export * from "./Input";
 export * from "./Loading";
 export * from "./LoadingWrapper";
 export * from "./NavBar";
-export * from "./Search";
 export * from "./TabBar";
 export * from "./Typography";
 
